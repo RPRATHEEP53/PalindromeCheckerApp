a6d7e239147917c8fe9be370cf49e021b2c90b9d
RPRATHEEP53 <pratheepraju2007@gmail.com>
2026-02-20 10:20:59 +0530

    Add files via upload

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,17 +1,23 @@
-/*
- * Application entry
- *
- * This is the first method exexuted by the JVM
- * When the Program starts
- *
- * @author Developer
- * @Version 1.0
- */
-
 public class PalindromeCheckerApp {
+
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized Successful");
+        // Step 1: Store a predefined string
+        String input = "madam" +
+                "";
+        boolean isPalindrome = true;
+
+
+        for (int i = 0; i < input.length() / 2; i++) {
+            // Compare character at index i with its mirrored character from the end
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break; // Exit loop early if a mismatch is found
+            }
+        }
+
+        // Step 3: Determine and display the result on the console
+        System.out.println("Input String: " + input);
+        System.out.println("is it a Palindrome? : " + isPalindrome);
     }
 }
